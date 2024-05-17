ac4e6bee4573c277d02d29f1d08ead19712794e4
OsmanBuyukcerci <osmanbuyukcerci17@gmail.com>
2024-05-17 17:06:24 +0300

    error fix

--- app/products/[page_number]/page.tsx
+++ app/products/[page_number]/page.tsx
@@ -5,7 +5,7 @@ export default function Home({ params }: any) {
   return (
     <main className="overflow-hidden flex flex-col items-center justify-center relative">
       {/* Products */}
-      <div className="grid grid-cols-4 gap-8 mb-8 mt-20 backdrop-blur-lg p-2">
+      <div className="grid grid-cols-4 gap-8 mb-8 mt-20 p-2">
         <Card />
         <Card />
         <Card />
